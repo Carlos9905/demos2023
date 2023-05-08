4d9bddf60546059fd9fb5f104ccf3462fb5e2e64
OscarRugama <77291834+OskrRugama@users.noreply.github.com>
2023-05-08 16:01:56 -0600

    price module

---
+++ precio_etd/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Precio ETD',
+    'version': '1.0.0',
+    'category': 'Sales',
+    'summary': 'Módulo para la actualización de precios de venta y tarifas',
+    'author': 'Oscar Rugama',
+    'website': 'https://www.ak.com.ni',
+    'depends': ['base', 'product', 'sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        #'views/product_template_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ precio_etd/models/product_product.py
@@ -0,0 +1,25 @@
+from odoo import api, fields, models
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    precio_etd_id = fields.Many2one(
+        'product.pricelist', string='Ubicación', ondelete='restrict',
+        help="Relación con la tarifa para actualizar el precio de venta y la tarifa.")
+
+    @api.onchange('precio_etd_id')
+    def _onchange_precio_etd_id(self):
+        if self.precio_etd_id:
+            self.list_price = self.precio_etd_id.get_product_price(self, 1, False)
+        else:
+            self.list_price = 0.0
+
+    @api.onchange('list_price')
+    def _onchange_list_price(self):
+        if self.precio_etd_id:
+            self.precio_etd_id.write({
+                'item_ids': [(0, 0, {
+                    'product_id': self.id,
+                    'fixed_price': self.list_price
+                })]
+            })
